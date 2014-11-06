be4825b2f91c100ec7fe5e2cc24031d7e26062af
Artur Kink <artur.kink@gmail.com>
2014-11-06 15:52:12 -0500

    Yo all functionality.

--- cyo.c
+++ cyo.c
@@ -4,11 +4,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * Curl response storing buffer.
+ */
 struct response_memory{
     char* response;
     size_t size;
 };
 
+/**
+ * Store curl response in given userptr.
+ */
 size_t curl_response(void* ptr, size_t size, size_t nmemb, void* userptr){
     size_t realsize = size * nmemb;
     struct response_memory *mem = (struct response_memory*)userptr;
@@ -24,14 +30,23 @@ size_t curl_response(void* ptr, size_t size, size_t nmemb, void* userptr){
     
     return realsize;
 }
+
+/**
+ * Convert response string from POST to enum code.
+ */
 int response_to_code(const char* response){
-    printf("response: %s\n", response);
+    //printf("Response: %s\n", response);
     if(strncmp(response, "{\"result\": \"OK\"}", 16) == 0){
+        return yo_ok;
+    }else if(strncmp(response, "{}", 2) == 0){
         return yo_ok;
     }
     return yo_fail;
 }
 
+/**
+ * Send yo to specific user.
+ */
 int yo(const char* username, const char* token){
     CURL* curl;
     curl = curl_easy_init();
@@ -39,7 +54,8 @@ int yo(const char* username, const char* token){
         curl_easy_setopt(curl, CURLOPT_POST, 1);
         curl_easy_setopt(curl, CURLOPT_URL, "https://api.justyo.co/yo/");
         
-        int post_len = 8 + 9 + 2 + 1 + strlen(username) + strlen(token);
+        //Create post arguments
+        int post_len = 20 + strlen(username) + strlen(token);
         char post_fields[post_len+1];
         strcpy(post_fields, "username=");
         strcat(post_fields, username);
@@ -47,6 +63,7 @@ int yo(const char* username, const char* token){
         strcat(post_fields, token);
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_fields);
 
+        //Response callback
         struct response_memory response;
         response.size = 0;
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
@@ -54,6 +71,8 @@ int yo(const char* username, const char* token){
 
         CURLcode res = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
+
+        //Check result
         if(res != CURLE_OK){
             return connection_fail;    
         }else if(response.size != 0){
@@ -65,6 +84,9 @@ int yo(const char* username, const char* token){
     return connection_fail;
 }
 
+/**
+ * Send link Yo to specific user.
+ */
 int yo_link(const char* username, const char* token, const char* link){
     CURL* curl;
     curl = curl_easy_init();
@@ -72,7 +94,8 @@ int yo_link(const char* username, const char* token, const char* link){
         curl_easy_setopt(curl, CURLOPT_POST, 1);
         curl_easy_setopt(curl, CURLOPT_URL, "https://api.justyo.co/yo/");
         
-        int post_len = 8 + 9 + 4 + 3 + 2 + strlen(username) + strlen(token) + strlen(link);
+        //Create post arguments
+        int post_len = 26 + strlen(username) + strlen(token) + strlen(link);
         char post_fields[post_len+1];
         strcpy(post_fields, "username=");
         strcat(post_fields, username);
@@ -82,6 +105,7 @@ int yo_link(const char* username, const char* token, const char* link){
         strcat(post_fields, link);
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_fields);
 
+        //Response callback
         struct response_memory response;
         response.size = 0;
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
@@ -89,6 +113,7 @@ int yo_link(const char* username, const char* token, const char* link){
 
         CURLcode res = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
+        //Check result
         if(res != CURLE_OK){
             return connection_fail;    
         }else if(response.size != 0){
@@ -100,6 +125,9 @@ int yo_link(const char* username, const char* token, const char* link){
     return connection_fail;
 }
 
+/**
+ * Send location Yo to specific user.
+ */
 int yo_location(const char* username, const char* token, const char* location){
     CURL* curl;
     curl = curl_easy_init();
@@ -107,7 +135,8 @@ int yo_location(const char* username, const char* token, const char* location){
         curl_easy_setopt(curl, CURLOPT_POST, 1);
         curl_easy_setopt(curl, CURLOPT_URL, "https://api.justyo.co/yo/");
         
-        int post_len = 8 + 9 + 8 + 3 + 2 + strlen(username) + strlen(token) + strlen(location);
+        //Create post arguments
+        int post_len = 30 + strlen(username) + strlen(token) + strlen(location);
         char post_fields[post_len+1];
         strcpy(post_fields, "username=");
         strcat(post_fields, username);
@@ -117,6 +146,7 @@ int yo_location(const char* username, const char* token, const char* location){
         strcat(post_fields, location);
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_fields);
 
+        //Response callback
         struct response_memory response;
         response.size = 0;
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
@@ -124,6 +154,7 @@ int yo_location(const char* username, const char* token, const char* location){
 
         CURLcode res = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
+        //Check result
         if(res != CURLE_OK){
             return connection_fail;    
         }else if(response.size != 0){
@@ -135,10 +166,78 @@ int yo_location(const char* username, const char* token, const char* location){
     return connection_fail;
 }
 
+/**
+ * Send Yo to all subscribers.
+ */
 int yo_all(const char* token){
+    CURL* curl;
+    curl = curl_easy_init();
+    if(curl){
+        curl_easy_setopt(curl, CURLOPT_POST, 1);
+        curl_easy_setopt(curl, CURLOPT_URL, "https://api.justyo.co/yoall/");
+        
+        //Create post arguments
+        int post_len = 10 + strlen(token);
+        char post_fields[post_len+1];
+        strcpy(post_fields, "api_token=");
+        strcat(post_fields, token);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_fields);
+
+        //Response callback
+        struct response_memory response;
+        response.size = 0;
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_response);
+
+        CURLcode res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+        //Check result
+        if(res != CURLE_OK){
+            return connection_fail;    
+        }else if(response.size != 0){
+            int response_code = response_to_code(response.response);
+            free(response.response);
+            return response_code;
+        }
+    }
     return connection_fail;
 }
 
+/**
+ * Send link Yo to all subscribers.
+ */
 int yo_all_link(const char* token, const char* link){
+    CURL* curl;
+    curl = curl_easy_init();
+    if(curl){
+        curl_easy_setopt(curl, CURLOPT_POST, 1);
+        curl_easy_setopt(curl, CURLOPT_URL, "https://api.justyo.co/yoall/");
+        
+        //Create post arguments
+        int post_len = 16 + strlen(token) + strlen(link);
+        char post_fields[post_len+1];
+        strcpy(post_fields, "api_token=");
+        strcat(post_fields, token);
+        strcat(post_fields, "&link=");
+        strcat(post_fields, link);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_fields);
+
+        //Response callback
+        struct response_memory response;
+        response.size = 0;
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_response);
+
+        CURLcode res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+        //Check result
+        if(res != CURLE_OK){
+            return connection_fail;    
+        }else if(response.size != 0){
+            int response_code = response_to_code(response.response);
+            free(response.response);
+            return response_code;
+        }
+    }
     return connection_fail;
 }
--- yo.c
+++ yo.c
@@ -1,37 +1,68 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "cyo.h"
 
+/**
+ * Print program usage.
+ */
 void printf_usage(){
-    printf("Usage: yo <username> <api_token>\n");
-    printf("Optional: Only 1 of the following\n");
-    printf("-u url\n");
-    printf("-l location\n");
+    printf("Usage: yo <api_token>\n");
+    printf("Optional:\n");
+    printf("-t <username> to send yo to, without this all subscribers will be Yo'd\n");
+    printf("-u <url>, can be sent to all or specific user\n");
+    printf("-l <location>, can only be sent to specific user\n");
 }
 
 int main(int argc, char** argv){
 
-    if(argc < 3){
+    //Min arguments check
+    if(argc < 2){
         printf_usage();
         return 1;
     }
 
-    int resp = 0;
-    if(argc == 3){
-        resp = yo(argv[1], argv[2]);
-    }else if(argc == 5){
-        if(strcmp(argv[3], "-u") == 0){
-            resp = yo_link(argv[1], argv[2], argv[4]);
-        }else if(strcmp(argv[3], "-l") == 0){
-            resp = yo_location(argv[1], argv[2], argv[4]);
-        }else{
-            printf_usage();
-            return 1;
+    char* token = argv[1];
+    int flag;
+    char* username = 0;
+    char* url = 0;
+    char* location = 0;
+
+    //Parse arg flags
+    while((flag = getopt(argc, argv, "t:u:l:")) != -1){
+        switch(flag){
+            case 't':
+                username = optarg;
+                break;
+            case 'u':
+                url = optarg;
+                break;
+            case 'l':
+                location = optarg;
+                break;
+            default:
+                printf_usage();
+                return 1;
+                break;
         }
-    }else{
-        printf_usage();
-        return 1;
     }
 
+    //Perform Yo
+    int resp = 0;
+    if(username == 0){
+        if(url == 0)
+            resp = yo_all(token);
+        else
+            resp = yo_all_link(token, url);
+    }else if(username){
+        if(url)
+            resp = yo_link(username, token, url);
+        else if(location)
+            resp = yo_location(username, token, location);
+        else
+            resp = yo(username, token);
+    }
+
+    //Print result
     if(resp == yo_ok){
         printf("Yo sent\n");
     }else{
